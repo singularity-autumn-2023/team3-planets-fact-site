fd99d06bb781ca70e9cbcdb90f1ecf7cc7d99d59
Spatayev <40237017+Spatayev@users.noreply.github.com>
2024-01-20 15:50:53 +0600

    deleted comments

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,15 +1,3 @@
-// import { error } from '@sveltejs/kit';
-
-// export async function load() {
-// 	const planetsData = await fetch('http://127.0.0.1:8081/api/v1/planets/');
-
-// 	if (planetsData) {
-// 		const planetsRes = planetsData.json();
-// 		return planetsRes;
-// 	}
-
-// 	error(404, 'Not found');
-// }
 export const load = async ({ fetch }) => {
 	const planetsRes = await fetch('http://127.0.0.1:8081/api/v1/planets/');
 	if (planetsRes) {
